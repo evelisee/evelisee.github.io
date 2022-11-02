1b403d7b2d0e845048dbe21f72f393ce007ce664
evelisee <evelisevazquez@gmail.com>
2022-11-01 22:37:39 -0300

    fix: next config

--- next.config.js
+++ next.config.js
@@ -3,14 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placekitten.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+   
   }
 }
 
